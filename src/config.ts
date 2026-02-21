8ab37c99d4b75592c710d4d6651becf6819eb52b
Arshvm <arshamnwwii708@gmail.com>
2026-02-21 20:04:06 +0330

    temp changes in about

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export const themeConfig: ThemeConfig = {
   // SITE INFO ///////////////////////////////////////////////////////////////////////////////////////////
   site: {
     website: 'https://thvrvz.vercel.app/', // Site domain
-    title: '</>', // Site title
+    title: 'Arshvm', // Site title
     author: 'Arshvm', // Author name
     description: 'Minimal blog built Arshvm', // Site description
     language: 'en-US' // Default language
--- src/content/about/about.md
+++ src/content/about/about.md
@@ -7,6 +7,4 @@ This content will be displayed at the top of the index page.
 You can leave this empty if you don’t want to show any content.
 -->
 
-## Arsham.H.Firouziyan 
-
-EES, Python Software developer, Junior designer 
+_<h2>EES, Python Software developer, Junior designer</h2>_
